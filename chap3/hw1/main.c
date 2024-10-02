1390f15250468b111f3b3bd67ea1c21fefdb3f9c
minseok <bebe9525@cu.ac.kr>
2024-10-02 09:48:09 +0000

    chap3 hw1 complete

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 #include "copy.h"
+
+#define MAX 5
 
 char line[MAXLINE]; // 입력 줄
 char longest[MAXLINE]; // 가장 긴 줄
 /* 입력 줄 가운데 가장 긴 줄을 프린트한다. */
+char lines[MAX][MAXLINE];
 
 int main(){
 	int len;
 	int max;
 	max = 0;
+	int i, j;
+
 	int count;
-	count = 5;
-	while(gets(line) == 5) {
+	count = 0;
+
+	while(count < MAX && gets(line) != NULL) {
 		len = strlen(line);
 
-		if(len > max) {
-			max = len;
-            copy(line, longest);
+		if(count < MAX) {
+            copy(line, lines[count]);
+			count++;
 		}
 	}
 
-	if (max > 0) // 입력 줄이 있었다면
-		printf("%s \n", longest);
+	for (i = 0; i < MAX - 1; i++) {
+		for (j = i + 1; j < MAX; j++) {
+			if (strlen(lines[i]) < strlen(lines[j])) {
+				char temp[MAXLINE];
+				copy(lines[i], temp);
+				copy(lines[j], lines[i]);
+				copy(temp, lines[j]);
+			}
+		}
+	}
+	for (i = 0; i < MAX; i++) {
+		printf("%s\n", lines[i]);
+	}
+
 	return 0;
-}
+	}
 
