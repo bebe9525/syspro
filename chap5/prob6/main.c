d7855662ee7041e27401f9d7f168e92fe296af6c
minseok <bebe9525@cu.ac.kr>
2024-10-16 13:27:38 +0000

    chap5/prob5, 6, 7 error. chap5/prob8, 9 complete

--- chap5/prob6/main.c
+++ chap5/prob6/main.c
@@ -4,29 +4,32 @@
 #include <fcntl.h>
 #include "student.h"
 
-int main(int argc, char *argv[]) {
-	int fd, id;
-	char c;
-	struct student record;
-	if(argc < 2) {
-		fprintf(stderr, "How to use : %s file\n", argv[0]);
-		exit(1);
-	}
-	if ((fd = open(argv[1], O_RDONLY)) == -1) {
-		perror(argv[1]);
-		exit(2);
-	}
-	do {
-		printf("검색할 학생의 학번 입력:");
-		if (scanf("%d", &id) == 1) {
-			lseek(fd, (id-START_ID)*sizeof(record), SEEK_SET);
-			if ((read(fd, (char *) &record, sizeof(record)) > 0) && (record.id != 0))
-				printf("Name:%s\t StuID:%d\t Score:%d\n", record.name, record.id, record.score);
-			else printf("레코드 %d 없음\n", id);
-		}else printf("입력 오류");
-		printf("계속하시겠습니까?(Y/N)");
-		scanf(" %c", &c);
-	} while (c == 'Y');
-	close(fd);
-	exit(0);
+int main(int argc, char *argv[])
+{
+   int fd, id;
+	char c; 
+   struct student record;
+   if (argc < 2) {
+      fprintf(stderr, "How to use : %s file\n", argv[0]);
+      exit(1);
+   }
+   if ((fd = open(argv[1], O_RDONLY)) == -1) {
+      perror(argv[1]);
+      exit(2);
+   }
+   do {
+      printf("\Enter StudentID to search:");
+      if (scanf("%d", &id) == 1) {
+         lseek(fd, (id-START_ID)*sizeof(record), SEEK_SET);
+          if ((read(fd, (char *) &record, sizeof(record)) > 0) && (record.id != 0))
+       printf("Name:%s\t StuID:%d\t Score:%d\n", record.name, record.id, record.score);
+         else printf("Record %d Null\n", id);
+      } else printf("Input Error");
+      printf("Continue?(Y/N)");
+      scanf(" %c", &c);
+   } while (c=='Y');
+   close(fd);
+   exit(0);
 }
+
+
